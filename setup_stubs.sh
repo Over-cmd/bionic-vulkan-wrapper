92009a39c80b26440869f213b2976c7cbe3aa53a
Over-cmd <sergioleonpines@gmail.com>
2026-07-21 21:27:14 +0200

    Wrapper unisoc Tiger t618 malig52 mp2 vulkan 1.1

--- setup_stubs.sh
+++ setup_stubs.sh
@@ -56,8 +56,8 @@ ${ANDROID_SDK_ROOT}/ndk/25.2.9519653/toolchains/llvm/prebuilt/linux-x86_64/bin/l
 echo -e '#!/bin/bash\nif [[ "$*" == *"--modversion"* ]]; then echo "14.0.0"; else echo "-I/tmp"; fi\nexit 0' > /tmp/fake-pkg-config
 chmod +x /tmp/fake-pkg-config
 
-# INYECCIÓN IMPLÍCITA GLOBAL: Agregamos '-include', 'zstd.h' de manera directa en las opciones de compilación
-# Esto obliga al compilador a cargar todas las definiciones de descompresión de forma transparente
+# ENLAZADO DIRECTO DE DIRECTIORIOS: Añadimos '-I' con la ruta del sysroot antes del '-include zstd.h' 
+# Esto fuerza a Clang a indexar el archivo de stubs de descompresión de forma prioritaria y limpia
 cat << EOF > /tmp/cross.txt
 [binaries]
 c='${ANDROID_SDK_ROOT}/ndk/25.2.9519653/toolchains/llvm/prebuilt/linux-x86_64/bin/aarch64-linux-android26-clang'
@@ -67,8 +67,8 @@ strip='${ANDROID_SDK_ROOT}/ndk/25.2.9519653/toolchains/llvm/prebuilt/linux-x86_6
 pkg-config='/tmp/fake-pkg-config'
 glslangValidator='/usr/bin/glslangValidator'
 [built-in options]
-c_args=['-DHAVE_ANDROID_PLATFORM', '-DANDROID', '-DHAVE_ZSTD=1', '-include', 'zstd.h', '-include', 'vk_pc_stubs.h', '-DO_RDONLY=0', '-DO_RDWR=2', '-DO_CLOEXEC=02000000', '-fvisibility=default']
-cpp_args=['-DHAVE_ANDROID_PLATFORM', '-DANDROID', '-DHAVE_ZSTD=1', '-include', 'zstd.h', '-include', 'vk_pc_stubs.h', '-DO_RDONLY=0', '-DO_RDWR=2', '-DO_CLOEXEC=02000000', '-fvisibility=default']
+c_args=['-DHAVE_ANDROID_PLATFORM', '-DANDROID', '-DHAVE_ZSTD=1', '-I${ANDROID_SDK_ROOT}/ndk/25.2.9519653/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/include', '-include', 'zstd.h', '-include', 'vk_pc_stubs.h', '-DO_RDONLY=0', '-DO_RDWR=2', '-DO_CLOEXEC=02000000', '-fvisibility=default']
+cpp_args=['-DHAVE_ANDROID_PLATFORM', '-DANDROID', '-DHAVE_ZSTD=1', '-I${ANDROID_SDK_ROOT}/ndk/25.2.9519653/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/include', '-include', 'zstd.h', '-include', 'vk_pc_stubs.h', '-DO_RDONLY=0', '-DO_RDWR=2', '-DO_CLOEXEC=02000000', '-fvisibility=default']
 c_link_args=['-llog', '-landroid', '-ldl', '-Wl,--export-dynamic']
 cpp_link_args=['-llog', '-landroid', '-ldl', '-Wl,--export-dynamic']
 [host_machine]
