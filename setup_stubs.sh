5a1fa78497dff8743bc104e78e4eb7c14304b343
Over-cmd <sergioleonpines@gmail.com>
2026-07-22 11:47:55 +0200

    Wrapper unisoc Tiger t618 malig52 mp2 vulkan 1.1
    Removed unnecessary creation of pthreadtypes.h and updated fake-pkg-config path.

--- setup_stubs.sh
+++ setup_stubs.sh
@@ -1,28 +1,18 @@
 #!/bin/bash
 set -e
 
-INC="${ANDROID_SDK_ROOT}/ndk/25.2.9519653/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/include"
+# Interceptor global fake-pkg-config
+echo -e '#!/bin/bash\nif [[ "$*" == *"--modversion"* ]]; then echo "14.0.0"; else echo "-I/tmp"; fi\nexit 0' > /tmp/fake-pkg-config
+chmod +x /tmp/fake-pkg-config
 
-# 1. bits/pthreadtypes.h obligatorio para wsi_common.c
-mkdir -p "$INC/bits"
-echo -e '#ifndef _BITS_PTHREADTYPES_H\n#define _BITS_PTHREADTYPES_H\n#endif' > "$INC/bits/pthreadtypes.h"
-
-# 2. Interceptor global fake-pkg-config
-sudo cat << 'EOF' > /usr/local/bin/fake-pkg-config
-#!/bin/bash
-if [[ "$*" == *"--modversion"* ]]; then echo '"14.0.0"'; else echo "-I${ANDROID_SDK_ROOT}/ndk/25.2.9519653/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/include"; fi
-exit 0
-EOF
-sudo chmod +x /usr/local/bin/fake-pkg-config
-
-# 3. Creación del entorno cruzado inyectando las directivas de inicialización de Mali y Android
-cat << EOF > /tmp/cross_64.txt
+# Creación del entorno cruzado inyectando de forma inapelable las directivas de Mali y Android KHR
+cat << EOF > /tmp/cross.txt
 [binaries]
 c='${ANDROID_SDK_ROOT}/ndk/25.2.9519653/toolchains/llvm/prebuilt/linux-x86_64/bin/aarch64-linux-android26-clang'
 cpp='${ANDROID_SDK_ROOT}/ndk/25.2.9519653/toolchains/llvm/prebuilt/linux-x86_64/bin/aarch64-linux-android26-clang++'
 ar='${ANDROID_SDK_ROOT}/ndk/25.2.9519653/toolchains/llvm/prebuilt/linux-x86_64/bin/llvm-ar'
 strip='${ANDROID_SDK_ROOT}/ndk/25.2.9519653/toolchains/llvm/prebuilt/linux-x86_64/bin/llvm-strip'
-pkg-config='/usr/local/bin/fake-pkg-config'
+pkg-config='/tmp/fake-pkg-config'
 glslangValidator='/usr/bin/glslangValidator'
 [built-in options]
 c_args=['-DHAVE_ANDROID_PLATFORM', '-DANDROID', '-DVK_USE_PLATFORM_ANDROID_KHR', '-DVK_EXPORT', '-include', '/tmp/vk_pc_stubs.h', '-DO_RDONLY=0', '-DO_RDWR=2', '-DO_CLOEXEC=02000000', '-fvisibility=default']
