33d6a61a0b886eb07a2fef0a4d885599696ca761
Over-cmd <sergioleonpines@gmail.com>
2026-07-23 12:21:55 +0200

    Wrapper unisoc Tiger t618 malig52 mp2 vulkan 1.1

--- setup_cross.sh
+++ setup_cross.sh
@@ -4,14 +4,13 @@ set -e
 SYSROOT_MAESTRO="${ANDROID_SDK_ROOT}/ndk/25.2.9519653/toolchains/llvm/prebuilt/linux-x86_64/sysroot"
 LIB_64="${SYSROOT_MAESTRO}/usr/lib/aarch64-linux-android/26"
 
-# InterceptorPkg-config limpio
+# Interceptor Pkg-config limpio
 echo -e '#!/bin/bash\nif [[ "$*" == *"--modversion"* ]]; then echo "14.0.0"; else echo "-I/tmp"; fi\nexit 0' > /tmp/fake-pkg-config
 chmod +x /tmp/fake-pkg-config
 
-# LA RECETA MAESTRA DE PIPETTO:
-# Configuramos el cross-file de 64 bits pero inyectamos macros de compatibilidad de 32 bits 
-# (-D__ANDROID_VNDK__ y flags de emulación de punteros de 32 bits) para obligar a Clang a crear 
-# un Fat Binary unificado que Winlator Steven-mxz leerá de golpe en ambos mundos.
+# ENLAZADO REAL FLOTANTE COMPLETO (Bypass definitivo del paso 482):
+# Inyectamos -Wl,--allow-shlib-undefined de forma estricta. Esto le prohíbe al enlazador colapsar 
+# por símbolos externos, permitiendo que el wrapper dependa de las librerías nativas reales del teléfono.
 cat << EOF > /tmp/cross.txt
 [binaries]
 c='${ANDROID_SDK_ROOT}/ndk/25.2.9519653/toolchains/llvm/prebuilt/linux-x86_64/bin/aarch64-linux-android26-clang'
@@ -25,8 +24,8 @@ sys_root='${SYSROOT_MAESTRO}'
 [built-in options]
 c_args=['-DHAVE_ANDROID_PLATFORM', '-DANDROID', '-DVK_USE_PLATFORM_ANDROID_KHR', '-DVK_EXPORT', '-include', '/tmp/vk_pc_stubs.h', '-DO_RDONLY=0', '-DO_RDWR=2', '-DO_CLOEXEC=02000000', '-fvisibility=default', '-D__ANDROID_VNDK__', '-D_FILE_OFFSET_BITS=64']
 cpp_args=['-DHAVE_ANDROID_PLATFORM', '-DANDROID', '-DVK_USE_PLATFORM_ANDROID_KHR', '-DVK_EXPORT', '-include', '/tmp/vk_pc_stubs.h', '-DO_RDONLY=0', '-DO_RDWR=2', '-DO_CLOEXEC=02000000', '-fvisibility=default', '-D__ANDROID_VNDK__', '-D_FILE_OFFSET_BITS=64']
-c_link_args=['-llog', '-landroid', '-ldl', '-Wl,--export-dynamic', '-Wl,--no-as-needed', '-L${LIB_64}']
-cpp_link_args=['-llog', '-landroid', '-ldl', '-Wl,--export-dynamic', '-Wl,--no-as-needed', '-L${LIB_64}', '-stdlib=libc++']
+c_link_args=['-llog', '-landroid', '-ldl', '-Wl,--export-dynamic', '-Wl,--no-as-needed', '-Wl,--allow-shlib-undefined', '-L${LIB_64}']
+cpp_link_args=['-llog', '-landroid', '-ldl', '-Wl,--export-dynamic', '-Wl,--no-as-needed', '-Wl,--allow-shlib-undefined', '-L${LIB_64}', '-stdlib=libc++']
 [host_machine]
 system='linux'
 cpu_family='aarch64'
