28659fd33e67098488eccf448bdcab7abbf85ff9
Over-cmd <sergioleonpines@gmail.com>
2026-07-23 13:28:42 +0200

    Wrapper unisoc Tiger t618 malig52 mp2 vulkan 1.1

--- setup_cross.sh
+++ setup_cross.sh
@@ -7,7 +7,9 @@ LIB_64="${SYSROOT_MAESTRO}/usr/lib/aarch64-linux-android/26"
 echo -e '#!/bin/bash\nif [[ "$*" == *"--modversion"* ]]; then echo "14.0.0"; else echo "-I/tmp"; fi\nexit 0' > /tmp/fake-pkg-config
 chmod +x /tmp/fake-pkg-config
 
-# ESCRITURA VERTICAL DEL ARCHIVO CRUZADO DE PIPETTO (Funde 32 y 64 bits de fábrica)
+# RECETA DE ENLAZADO HÍBRIDO DE PIPETTO CON ZLIB NATIVA (Bypass definitivo de la línea 1582):
+# Forzamos la bandera -lz de forma explícita en los argumentos de enlace para que Meson use la 
+# librería real del sistema Android y deje de exigir la descarga o caída del subproyecto zlib.
 cat << EOF > /tmp/cross.txt
 [binaries]
 c='${ANDROID_SDK_ROOT}/ndk/25.2.9519653/toolchains/llvm/prebuilt/linux-x86_64/bin/aarch64-linux-android26-clang'
@@ -21,8 +23,8 @@ sys_root='${SYSROOT_MAESTRO}'
 [built-in options]
 c_args=['-DHAVE_ANDROID_PLATFORM', '-DANDROID', '-DVK_USE_PLATFORM_ANDROID_KHR', '-DVK_EXPORT', '-include', '/tmp/vk_pc_stubs.h', '-DO_RDONLY=0', '-DO_RDWR=2', '-DO_CLOEXEC=02000000', '-fvisibility=default', '-D__ANDROID_VNDK__', '-D_FILE_OFFSET_BITS=64']
 cpp_args=['-DHAVE_ANDROID_PLATFORM', '-DANDROID', '-DVK_USE_PLATFORM_ANDROID_KHR', '-DVK_EXPORT', '-include', '/tmp/vk_pc_stubs.h', '-DO_RDONLY=0', '-DO_RDWR=2', '-DO_CLOEXEC=02000000', '-fvisibility=default', '-D__ANDROID_VNDK__', '-D_FILE_OFFSET_BITS=64']
-c_link_args=['-llog', '-landroid', '-ldl', '-Wl,--export-dynamic', '-Wl,--no-as-needed', '-L${LIB_64}']
-cpp_link_args=['-llog', '-landroid', '-ldl', '-Wl,--export-dynamic', '-Wl,--no-as-needed', '-L${LIB_64}', '-stdlib=libc++']
+c_link_args=['-llog', '-landroid', '-ldl', '-lz', '-Wl,--export-dynamic', '-Wl,--no-as-needed', '-L${LIB_64}']
+cpp_link_args=['-llog', '-landroid', '-ldl', '-lz', '-Wl,--export-dynamic', '-Wl,--no-as-needed', '-L${LIB_64}', '-stdlib=libc++']
 [host_machine]
 system='linux'
 cpu_family='aarch64'
