e2ed645a4b7198261d3cb1ae588879c048b7550a
Over-cmd <sergioleonpines@gmail.com>
2026-07-23 14:57:00 +0200

    Wrapper unisoc Tiger t618 malig52 mp2 vulkan 1.1
    Removed the creation of subproject directories and wrap files for SPIRV-Tools and SPIRV-Headers.

--- setup_libs_stubs.sh
+++ setup_libs_stubs.sh
@@ -1,23 +1,4 @@
 #!/bin/bash
 set -e
-
-# Creamos la carpeta de subproyectos dentro del árbol que se va a compilar
-mkdir -p "$GITHUB_WORKSPACE/wrapper_src/subprojects"
-
-# Escribimos los archivos .wrap con los hashes originales verificados de Khronos
-cat << 'EOF' > "$GITHUB_WORKSPACE/wrapper_src/subprojects/spirv-tools.wrap"
-[wrap-file]
-directory = SPIRV-Tools-2024.1
-source_url = https://github.com
-source_filename = v2024.1.tar.gz
-source_hash = 693a105f9c46d328c68aa2f89552b028da841029df4b0351336423a2a6b22b10
-patch_directory = spirv-tools
-EOF
-
-cat << 'EOF' > "$GITHUB_WORKSPACE/wrapper_src/subprojects/spirv-headers.wrap"
-[wrap-file]
-directory = SPIRV-Headers-2024.1
-source_url = https://github.com
-source_filename = v2024.1-headers.tar.gz
-source_hash = a2fb9e8b15d96200236e760bf0bfbaee49d5926c8bda32f91dfc1409f98a2872
-EOF
+# Los subproyectos originales de Leegao se procesan completos desde la raíz.
+exit 0
