08d7d9e0b60d2aac9329bcfd9749a5dd926255d1
Over-cmd <sergioleonpines@gmail.com>
2026-07-23 10:57:00 +0200

    Wrapper unisoc Tiger t618 malig52 mp2 vulkan 1.1

--- setup_cross.sh
+++ setup_cross.sh
@@ -2,12 +2,15 @@
 set -e
 
 SYSROOT_MAESTRO="${ANDROID_SDK_ROOT}/ndk/25.2.9519653/toolchains/llvm/prebuilt/linux-x86_64/sysroot"
+LIB_64="${SYSROOT_MAESTRO}/usr/lib/aarch64-linux-android/26"
+LIB_32="${SYSROOT_MAESTRO}/usr/lib/arm-linux-androideabi/26"
 
+# Interceptor real limpio libre de contaminación de Ubuntu
 echo -e '#!/bin/bash\nif [[ "$*" == *"--modversion"* ]]; then echo "14.0.0"; else echo "-I/tmp"; fi\nexit 0' > /tmp/fake-pkg-config
 chmod +x /tmp/fake-pkg-config
 
-# Escribimos cross.txt en líneas estrictamente verticales individuales (Soluciona error del lexer)
-cat << EOF > /tmp/cross.txt
+# ESCRITURA VERTICAL DEL ARCHIVO CRUZADO DE 64 BITS
+cat << EOF > /tmp/cross_64.txt
 [binaries]
 c='${ANDROID_SDK_ROOT}/ndk/25.2.9519653/toolchains/llvm/prebuilt/linux-x86_64/bin/aarch64-linux-android26-clang'
 cpp='${ANDROID_SDK_ROOT}/ndk/25.2.9519653/toolchains/llvm/prebuilt/linux-x86_64/bin/aarch64-linux-android26-clang++'
@@ -20,11 +23,34 @@ sys_root='${SYSROOT_MAESTRO}'
 [built-in options]
 c_args=['-DHAVE_ANDROID_PLATFORM', '-DANDROID', '-DVK_USE_PLATFORM_ANDROID_KHR', '-DVK_EXPORT', '-include', '/tmp/vk_pc_stubs.h', '-DO_RDONLY=0', '-DO_RDWR=2', '-DO_CLOEXEC=02000000', '-fvisibility=default']
 cpp_args=['-DHAVE_ANDROID_PLATFORM', '-DANDROID', '-DVK_USE_PLATFORM_ANDROID_KHR', '-DVK_EXPORT', '-include', '/tmp/vk_pc_stubs.h', '-DO_RDONLY=0', '-DO_RDWR=2', '-DO_CLOEXEC=02000000', '-fvisibility=default']
-c_link_args=['-llog', '-landroid', '-ldl', '-Wl,--export-dynamic']
-cpp_link_args=['-llog', '-landroid', '-ldl', '-Wl,--export-dynamic', '-stdlib=libc++']
+c_link_args=['-llog', '-landroid', '-ldl', '-Wl,--export-dynamic', '-Wl,--no-as-needed', '-L${LIB_64}']
+cpp_link_args=['-llog', '-landroid', '-ldl', '-Wl,--export-dynamic', '-Wl,--no-as-needed', '-L${LIB_64}', '-stdlib=libc++']
 [host_machine]
 system='linux'
 cpu_family='aarch64'
 cpu='armv8-a'
+endian='little'
+EOF
+
+# ESCRITURA VERTICAL DEL ARCHIVO CRUZADO DE 32 BITS
+cat << EOF > /tmp/cross_32.txt
+[binaries]
+c='${ANDROID_SDK_ROOT}/ndk/25.2.9519653/toolchains/llvm/prebuilt/linux-x86_64/bin/armv7a-linux-androideabi26-clang'
+cpp='${ANDROID_SDK_ROOT}/ndk/25.2.9519653/toolchains/llvm/prebuilt/linux-x86_64/bin/armv7a-linux-androideabi26-clang++'
+ar='${ANDROID_SDK_ROOT}/ndk/25.2.9519653/toolchains/llvm/prebuilt/linux-x86_64/bin/llvm-ar'
+strip='${ANDROID_SDK_ROOT}/ndk/25.2.9519653/toolchains/llvm/prebuilt/linux-x86_64/bin/llvm-strip'
+pkg-config='/tmp/fake-pkg-config'
+glslangValidator='/usr/bin/glslangValidator'
+[properties]
+sys_root='${SYSROOT_MAESTRO}'
+[built-in options]
+c_args=['-DHAVE_ANDROID_PLATFORM', '-DANDROID', '-DVK_USE_PLATFORM_ANDROID_KHR', '-DVK_EXPORT', '-include', '/tmp/vk_pc_stubs.h', '-DO_RDONLY=0', '-DO_RDWR=2', '-DO_CLOEXEC=02000000', '-fvisibility=default']
+cpp_args=['-DHAVE_ANDROID_PLATFORM', '-DANDROID', '-DVK_USE_PLATFORM_ANDROID_KHR', '-DVK_EXPORT', '-include', '/tmp/vk_pc_stubs.h', '-DO_RDONLY=0', '-DO_RDWR=2', '-DO_CLOEXEC=02000000', '-fvisibility=default']
+c_link_args=['-llog', '-landroid', '-ldl', '-Wl,--export-dynamic', '-Wl,--no-as-needed', '-L${LIB_32}']
+cpp_link_args=['-llog', '-landroid', '-ldl', '-Wl,--export-dynamic', '-Wl,--no-as-needed', '-L${LIB_32}', '-stdlib=libc++']
+[host_machine]
+system='linux'
+cpu_family='arm'
+cpu='armv7-a'
 endian='little'
 EOF
