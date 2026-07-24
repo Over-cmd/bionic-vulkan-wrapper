5db50d517b3cf23d1d1b6428b0137bf127fb588d
Over-cmd <sergioleonpines@gmail.com>
2026-07-24 14:52:34 +0200

    Wrapper unisoc Tiger t618 malig52 mp2 vulkan 1.1
    Updated Android platform version from 25 to 26 for both 32-bit and 64-bit builds. Added logic to copy libraries to multiple sysroot paths.

--- setup_spirv.sh
+++ setup_spirv.sh
@@ -8,15 +8,20 @@ mkdir -p spirv_source/build_32 && cd spirv_source/build_32
 cmake .. -G Ninja \
   -DCMAKE_TOOLCHAIN_FILE=$NDK_PATH/build/cmake/android.toolchain.cmake \
   -DANDROID_ABI=armeabi-v7a \
-  -DANDROID_PLATFORM=android-25 \
+  -DANDROID_PLATFORM=android-26 \
   -DCMAKE_BUILD_TYPE=Release \
   -DSPIRV_SKIP_TESTS=ON \
   -DSPIRV_WERROR=OFF
 ninja
 
-SYSROOT_32="$NDK_PATH/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/lib/arm-linux-androideabi/25"
-cp source/opt/libSPIRV-Tools-opt.a $SYSROOT_32/libSPIRV-Tools-opt.a
-cp source/libSPIRV-Tools.a $SYSROOT_32/libSPIRV-Tools.a
+# Inyectamos en todas las rutas posibles de 32 bits del NDK para blindar el find_library
+SYSROOT_32_BASE="$NDK_PATH/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/lib/arm-linux-androideabi"
+mkdir -p "$SYSROOT_32_BASE" "$SYSROOT_32_BASE/25" "$SYSROOT_32_BASE/26"
+
+for dest in "$SYSROOT_32_BASE" "$SYSROOT_32_BASE/25" "$SYSROOT_32_BASE/26"; do
+  cp source/opt/libSPIRV-Tools-opt.a "$dest/libSPIRV-Tools-opt.a"
+  cp source/libSPIRV-Tools.a "$dest/libSPIRV-Tools.a"
+done
 cd "$BASE_PWD"
 
 echo "=== 2. Compilando SPIRV-Tools Real para ARM64 (64 bits) ==="
@@ -24,13 +29,18 @@ mkdir -p spirv_source/build_64 && cd spirv_source/build_64
 cmake .. -G Ninja \
   -DCMAKE_TOOLCHAIN_FILE=$NDK_PATH/build/cmake/android.toolchain.cmake \
   -DANDROID_ABI=arm64-v8a \
-  -DANDROID_PLATFORM=android-25 \
+  -DANDROID_PLATFORM=android-26 \
   -DCMAKE_BUILD_TYPE=Release \
   -DSPIRV_SKIP_TESTS=ON \
   -DSPIRV_WERROR=OFF
 ninja
 
-SYSROOT_64="$NDK_PATH/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/lib/aarch64-linux-android/25"
-cp source/opt/libSPIRV-Tools-opt.a $SYSROOT_64/libSPIRV-Tools-opt.a
-cp source/libSPIRV-Tools.a $SYSROOT_64/libSPIRV-Tools.a
+# Inyectamos en todas las rutas posibles de 64 bits del NDK
+SYSROOT_64_BASE="$NDK_PATH/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/lib/aarch64-linux-android"
+mkdir -p "$SYSROOT_64_BASE" "$SYSROOT_64_BASE/25" "$SYSROOT_64_BASE/26"
+
+for dest in "$SYSROOT_64_BASE" "$SYSROOT_64_BASE/25" "$SYSROOT_64_BASE/26"; do
+  cp source/opt/libSPIRV-Tools-opt.a "$dest/libSPIRV-Tools-opt.a"
+  cp source/libSPIRV-Tools.a "$dest/libSPIRV-Tools.a"
+done
 cd "$BASE_PWD"
