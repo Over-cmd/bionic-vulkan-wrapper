30ef15b1cdcac8b037930427a3644cdbd6ba81bf
Over-cmd <sergioleonpines@gmail.com>
2026-07-29 11:06:10 +0200

    Wrapper unisoc Tiger t618 malig52 mp2 vulkan 1.1
    Removed echo statement indicating successful environment setup.

--- preparar_entorno.sh
+++ preparar_entorno.sh
@@ -11,8 +11,6 @@ mkdir -p local_pkgconfig
 printf "prefix=%s\nlibdir=%s\nincludedir=%s/local_include\n\nName: libdrm\nDescription: Userspace interface to kernel DRM services\nVersion: 2.4.120\nLibs: -L\${libdir} -ldrm\nCflags: -I\${includedir} -I\${includedir}/libdrm\n" "$BASE_PWD" "$BASE_PWD/build_drm" "$BASE_PWD" > local_pkgconfig/libdrm.pc
 printf "Name: SPIRV-Tools\nVersion: 2024.1\nLibs: -L$BASE_PWD/spirv_source/build_64/source -lSPIRV-Tools\n" > local_pkgconfig/SPIRV-Tools.pc
 printf "Name: SPIRV-Tools-opt\nVersion: 2024.1\nLibs: -L$BASE_PWD/spirv_source/build_64/source/opt -lSPIRV-Tools-opt\n" > local_pkgconfig/SPIRV-Tools-opt.pc
-
-# Inyectamos el mapa Pkg-Config oficial para glslang, forzando a Meson a rellenar la variable original de fábrica
 printf "Name: glslang\nVersion: 14.0.0\nLibs: -L$BASE_PWD/glslang_source/build_64/glslang -lglslang\nCflags: -I$BASE_PWD/glslang_source\n" > local_pkgconfig/glslang.pc
 
 if [ -f "$BASE_PWD/build_drm/libdrm.so" ]; then
@@ -21,4 +19,3 @@ if [ -f "$BASE_PWD/build_drm/libdrm.so" ]; then
 fi
 
 cp -f "$NDK_LIB_DIR_64/libadrenotools.a" "$NDK_LIB_DIR_32/libadrenotools.a" 2>/dev/null || true
-echo "=== ENTORNO ENLAZADOR TOTALMENTE CONECTADO CON GLSLANG DE ORIGEN ==="
