bdac77ba3c45a75812f1d895c7362c9259410d1f
Over-cmd <sergioleonpines@gmail.com>
2026-08-03 07:41:01 +0200

    Wrapper unisoc Tiger t618 malig52 mp2 vulkan 1.1

--- preparar_entorno.sh
+++ preparar_entorno.sh
@@ -58,6 +58,7 @@ strip = '/bin/true'
 pkg-config = '/usr/bin/pkg-config'
 glslangValidator = '/usr/bin/glslangValidator'
 [built-in options]
+b_lundef = false
 c_args = ['-w', '-D_GNU_SOURCE', '-I$BASE_PWD/local_include', '-I$BASE_PWD/local_include/libdrm', '-I$BASE_PWD/spirv_source/include', '-I$BASE_PWD/glslang_source', '-I$BASE_PWD/adrenotools_source/include', '-march=armv7-a', '-mfloat-abi=hard', '-mfpu=neon', '-I$SYSROOT_PATH/usr/include']
 cpp_args = ['-w', '-D_GNU_SOURCE', '-I$BASE_PWD/local_include', '-I$BASE_PWD/local_include/libdrm', '-I$BASE_PWD/spirv_source/include', '-I$BASE_PWD/glslang_source', '-I$BASE_PWD/adrenotools_source/include', '-march=armv7-a', '-mfloat-abi=hard', '-mfpu=neon', '-I$SYSROOT_PATH/usr/include']
 c_link_args = ['-fuse-ld=lld', '-L$NDK_LIB_DIR_32', '-lc', '-lm', '-ldl', '-llog', '-landroid']
