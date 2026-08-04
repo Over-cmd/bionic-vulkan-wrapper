233f910f71c0d978eb83e6a304979b34a53fe0d5
Over-cmd <sergioleonpines@gmail.com>
2026-08-04 18:35:31 +0200

    Wrapper unisoc Tiger t618 malig52 mp2 vulkan 1.1

--- preparar_entorno.sh
+++ preparar_entorno.sh
@@ -2,21 +2,19 @@
 set -e
 echo "=== ETAPA C-2: PREPARACIÓN DE ENTORNO EN CALIENTE (OVER-CMD) ==="
 
-BASE_PWD="/workspace"
-
-# Forjamos el archivo cross64.txt con los compiladores globales del contenedor Docker
+# Forjamos el archivo cross64.txt usando el compilador clang directo del contenedor
 cat << 'EOF' > cross64.txt
 [binaries]
-c = 'aarch64-linux-android-clang'
-cpp = 'aarch64-linux-android-clang++'
+c = 'clang'
+cpp = 'clang++'
 ar = 'llvm-ar'
 strip = 'llvm-strip'
 pkg-config = '/usr/bin/pkg-config'
 [built-in options]
-c_args = ['-w', '-D_GNU_SOURCE', '-I/workspace/local_include', '-I/workspace/local_include/libdrm', '-I/workspace/spirv_source/include']
-cpp_args = ['-w', '-D_GNU_SOURCE', '-I/workspace/local_include', '-I/workspace/local_include/libdrm', '-I/workspace/spirv_source/include']
-c_link_args = ['-lc', '-llog', '-landroid', '-ldl']
-cpp_link_args = ['-lc', '-llog', '-landroid', '-ldl']
+c_args = ['-target', 'aarch64-linux-android26', '-w', '-D_GNU_SOURCE', '-I/workspace/local_include', '-I/workspace/local_include/libdrm', '-I/workspace/spirv_source/include']
+cpp_args = ['-target', 'aarch64-linux-android26', '-w', '-D_GNU_SOURCE', '-I/workspace/local_include', '-I/workspace/local_include/libdrm', '-I/workspace/spirv_source/include']
+c_link_args = ['-target', 'aarch64-linux-android26', '-lc', '-llog', '-landroid', '-ldl']
+cpp_link_args = ['-target', 'aarch64-linux-android26', '-lc', '-llog', '-landroid', '-ldl']
 [host_machine]
 system = 'android'
 cpu_family = 'aarch64'
